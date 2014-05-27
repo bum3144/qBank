3e66915852e6b8cf2d1cc7e376458508d28596b6
Ratist <ura0508@hanmail.net>
2014-05-27 18:10:32 +0900

    .

--- qBank/src/main/webapp/js/joinForm.js
+++ qBank/src/main/webapp/js/joinForm.js
@@ -10,51 +10,29 @@ qbank.contextRoot = qbank.getContextRootPath();
 
 $(document).ready(function(){
 	
-	$('#joinForm').load(qbank.contextRoot + '/joinForm.html');
-	$("#popup_content1").css({display:"none"});
-	$("#popup_content2").css({display:"none"});
-	$("#popup_content3").css({display:""});
-	$('#selectBtn').on('click',function(){
-		$("#popup_content3").css({display:""});
+	$("#content1").css({display:""});
+	$("#content2").css({display:"none"});
+	$("#content3").css({display:"none"});
+	
+	$('.nextBtn1').on('click',function(){
+		$("#content1").css({display:"none"});
+		$("#content2").css({display:""});
+		$("#content3").css({display:"none"});
 		
 	});
-//	
-//	$.ajax('insert.ajax', {
-//		type: 'POST',
-//		dataType: 'json', /*서버에서 보내는 데이터의 형식 지정 */
-//		data: { /* 서버쪽으로 보내는 데이터 */
-//			uclass: $('#uclass').val(),
-//			uid: $('#uid').val(),
-//			password: $('#password').val(),
-//			name: $('#name').val(),
-//			sex: $('#sex').val(),
-//			birth: $('#birth').val(),
-//			tel: $('#tel').val(),
-//			cell: $('#cell').val(),
-//			email: $('#email').val(),
-//			zipcode: $('#zipcode').val(),
-//			addr1: $('#addr1').val(),
-//			addr2: $('#addr2').val(),
-//			
-//			
-//		},
-//		success: function(jsonObj){
-//			console.log(jsonObj);
-//			var result = jsonObj.ajaxResult;
-//			if (result.status == "ok" && result.data == "success") {
-//				location.href="../main2.html";
-//			} else {
-//				alert('result status data 안맞아~~~');
-//			}
-//		},
-//		error: function(xhr, status, errorThrown){
-//			alert('회원가입 실행 중 오류 발생!');
-//			console.log(status);
-//			console.log(errorThrown);
-//		}
-//		
-//	});
 	
+	$('.nextBtn2').on('click',function(){
+		$("#content1").css({display:"none"});
+		$("#content2").css({display:"none"});
+		$("#content3").css({display:""});
+		
+	});
+	
+	$('#nextBtn3').on('click',function(){
+		$("#content1").css({display:""});
+		$("#content2").css({display:"none"});
+		$("#content3").css({display:"none"});
+	});
 });
 
 
