1cae5c64a6f87c2a886735554569e7f3bb7e1760
Ratist <ura0508@hanmail.net>
2014-06-02 17:33:18 +0900

    .

--- qBank/src/main/java/qbank/vo/UserVo.java
+++ qBank/src/main/java/qbank/vo/UserVo.java
@@ -10,51 +10,109 @@ public class UserVo implements Serializable{
   String	uid;
   String 	upass;
   String 	uname;
+  String  usex;
+  String  ubirth;
   String 	uemail;
+  String  utel;
+  String  ucellphone;
+  String  uzipcode;
+  String  uaddr1;
+  String  uaddr2;
   
   
-public int getUno() {
-	return uno;
-}
-public UserVo setUno(int uno) {
-	this.uno = uno;
-	return this;
-}
-public String getUclass() {
-	return uclass;
-}
-public UserVo setUclass(String uclass) {
-	this.uclass = uclass;
-	return this;
-}
-public String getUid() {
-	return uid;
-}
-public UserVo setUid(String uid) {
-	this.uid = uid;
-	return this;
-}
-public String getUpass() {
-	return upass;
-}
-public UserVo setUpass(String upass) {
-	this.upass = upass;
-	return this;
-}
-public String getUname() {
-	return uname;
-}
-public UserVo setUname(String uname) {
-	this.uname = uname;
-	return this;
-}
-public String getUemail() {
-	return uemail;
-}
-public UserVo setUemail(String uemail) {
-	this.uemail = uemail;
-	return this;
-}  
+	public int getUno() {
+		return uno;
+	}
+	public UserVo setUno(int uno) {
+		this.uno = uno;
+		return this;
+	}
+	public String getUclass() {
+		return uclass;
+	}
+	public UserVo setUclass(String uclass) {
+		this.uclass = uclass;
+		return this;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public UserVo setUid(String uid) {
+		this.uid = uid;
+		return this;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public UserVo setUpass(String upass) {
+		this.upass = upass;
+		return this;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public UserVo setUname(String uname) {
+		this.uname = uname;
+		return this;
+	}
+	public String getUsex() {
+		return usex;
+	}
+	public UserVo setUsex(String usex) {
+		this.usex = usex;
+		return this;
+	}
+	public String getUbirth() {
+		return ubirth;
+	}
+	public UserVo setUbirth(String ubirth) {
+		this.ubirth = ubirth;
+		return this;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public UserVo setUemail(String uemail) {
+		this.uemail = uemail;
+		return this;
+	}
+	public String getUtel() {
+		return utel;
+	}
+	public UserVo setUtel(String utel) {
+		this.utel = utel;
+		return this;
+	}
+	public String getUcellphone() {
+		return ucellphone;
+	}
+	public UserVo setUcellphone(String ucellphone) {
+		this.ucellphone = ucellphone;
+		return this;
+	}
+	public String getUzipcode() {
+		return uzipcode;
+	}
+	public UserVo setUzipcode(String uzipcode) {
+		this.uzipcode = uzipcode;
+		return this;
+	}
+	public String getUaddr1() {
+		return uaddr1;
+	}
+	public UserVo setUaddr1(String uaddr1) {
+		this.uaddr1 = uaddr1;
+		return this;
+	}
+	public String getUaddr2() {
+		return uaddr2;
+	}
+	public UserVo setUaddr2(String uaddr2) {
+		this.uaddr2 = uaddr2;
+		return this;
+	}
+
+  
   
 }
 
