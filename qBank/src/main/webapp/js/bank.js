a1c429fa27215b6975339fb6fda3a7fa58539867
Ratist <ura0508@hanmail.net>
2014-06-12 20:21:11 +0900

    .

--- qBank/src/main/webapp/js/bank.js
+++ qBank/src/main/webapp/js/bank.js
@@ -1,61 +1,6 @@
 
 	$(document).ready(function() {
 		
-
-		
-		
-/*
-		$("#checkAll").click(function(){
-			if ($("#checkAll").is(":checked")) {	
-				$("input[name=box]:checkbox").each(function() {
-					$(this).attr("checked", true);
-					$("#checkAll").attr("checked", true);
-				});
-			}else{
-				$("input[name=box]:checkbox").each(function() {
-					$(this).attr("checked", false);
-					$("#checkAll").attr("checked", false);
-				});	
-			}
-		});
-*/
-		/*		
-		// 체크 박스 모두 체크
-		$("#checkAll").click(function() {
-			$("input[name=box]:checkbox").each(function() {
-				$(this).attr("checked", true);
-			});
-		});
-
-		// 체크 박스 모두 해제
-		$("#uncheckAll").click(function() {
-			$("input[name=box]:checkbox").each(function() {
-				$(this).attr("checked", false);
-			});
-		});*/
-
-		
-		
-/*		// 체크 되어 있는 값 추출
-		$("#getCheckedAll").click(function() {
-			$("input[name=box]:checked").each(function() {
-				var test = $(this).val();
-				console.log(test);
-			});
-		});
-
-		// 서버에서 받아온 데이터 체크하기 (콤마로 받아온 경우)
-		$("#updateChecked").click(function() {
-			var splitCode = $("#splitCode").val().split(",");
-			for (var idx in splitCode) {
-				$("input[name=box][value=" + splitCode[idx] + "]").attr("checked", true);
-			}
-		});
-
-		// test case
-		test1();
-*/
-		
 		/*  문제 등록 선택체크 */
 		
 		var array = [ 
@@ -116,8 +61,24 @@
 		/* 보기창 숨김 보임 끝*/
 		
 		
+		/* 문형 선택창 */
+		
+		$('#answerBox').hide();
+		$('#typeSelector').change(function(){
+			if($(this).val() == '객관식'){
+				$('#exSelectBox').show();
+				$('#answerBox').hide();
+			}else{
+				$('#exSelectBox').hide();
+				$('#answerBox').show();
+			}
+		});
+		
+		/* 문형 선택창 끝 */
+		
+		
 		/* 미리보기창 띄우기 */
-		 //$( "#toPopup" ).draggable();
+		 //$( "#previewBtn" ).draggable();
 
 			$("#previewBtn").click(function() {
 					setTimeout(function(){ // then show popup, deley in .5 second
@@ -142,7 +103,15 @@
 				disablePopup();  // function close pop up
 			});
 
-
+		        /* 미리보기창 띄우기 */
+		        
+		        
+		   /* 
+		        
+		        
+		        
+		        
+		        
 
 			 /************** start: functions. **************/
 			
@@ -164,35 +133,9 @@
 					popupStatus = 0;  // and set value to 0
 				}
 			}
-			/************** end: functions. **************/
 	
-	
-		
-		
 
-	function test1() {
-
-		console.log("################################################");
-		console.log("## test1 START");
-		console.log("################################################");
-
-		var cnt = $("input:checkbox").size();
-		console.log("checkboxSize=" + cnt);
-
-		$("input[name=box]:checkbox").each(function() {
-			var checkboxValue = $(this).val();
-			console.log("checkboxValue=" + checkboxValue);
-		});
-
-		console.log("----------------------------------------------");
-
-		$("#checkboxArea").children().each(function() {
-			var checkboxValue = $(this).children(":checkbox").val();
-			var text = $(this).children().eq(1).text();
-			console.log(text + "=" + checkboxValue);
-		});
-	}
-	
+	/************** end: functions. **************/
 	
 	});
 	
