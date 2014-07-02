ff5816bf5e822a9c5e4a585825a2611b4bfc84e7
Jeremy.Park <bum3144@gmail.com>
2014-07-02 22:56:20 +0900

    .

--- qBank/src/main/java/qbank/vo/BankAddVo.java
+++ qBank/src/main/java/qbank/vo/BankAddVo.java
@@ -1,23 +1,30 @@
 package qbank.vo;
 
 public class BankAddVo {
-	private int 			qno;		//문제번호
-	private String 			qcode;		//문제코드
-	private String			qtype;		//문제유형
-	private String 			qlevel;  	//난이도
-	private String 	 		qedituse;	//에디터사용유무
-	private String 	 		qname;		//문제명	
-	private boolean 		textCheck;	//지문 입력 체크	
-	private String 	 		qtext;		//문제 지문
-	private boolean 		commentCheck;	//문제 해설 체크
-	private String 	 		qcomment;	//문제 해설
-	private boolean		sourceCheck;	//문제 출처 체크
-	private String			qsource;	//문제 출처
-	private String			quseyn;		//사용 유무
-	private String			qwriter;	//작성자
-	private String 	 		qdate;		//작성일
-	private int 	 		ccode;		//카테고리 번호
+	private int 			qno;				//문제번호
+	private String 			qcode;				//문제코드
+	private String			typeSelector;		//문제유형
+	private String 			level;  			//난이도
+	private String 	 		qedituse;			//에디터사용유무
+	private String 	 		questVal;			//문제명	
+	private boolean 		textCheck;			//지문 입력 체크	
+	private String 	 		textDisplay;		//문제 지문	
+	private boolean 		comentCheck;		//문제 해설 체크
+	private String 	 		comentDisplay;		//문제 해설
+	private boolean		sourceCheck;		//문제 출처 체크
+	private String			sourceDisplay;		//문제 출처
+	private String			quseyn;				//사용 유무
+	private String			qwriter;			//작성자
+	private String 	 		qdate;				//작성일
+	private int 	 		cCode;				//카테고리 번호
 	
+	public BankAddVo() {
+		this.qedituse = "N";
+		this.textCheck = false;
+		this.comentCheck = false;
+		this.sourceCheck = false;
+		this.quseyn = "Y";
+	}
 	
 	public int getQno() {
 		return qno;
@@ -33,18 +40,18 @@ public class BankAddVo {
 		this.qcode = qcode;
 		return this;
 	}
-	public String getQtype() {
-		return qtype;
+	public String getTypeSelector() {
+		return typeSelector;
 	}
-	public BankAddVo setQtype(String qtype) {
-		this.qtype = qtype;
+	public BankAddVo setTypeSelector(String typeSelector) {
+		this.typeSelector = typeSelector;
 		return this;
 	}
-	public String getQlevel() {
-		return qlevel;
+	public String getLevel() {
+		return level;
 	}
-	public BankAddVo setQlevel(String qlevel) {
-		this.qlevel = qlevel;
+	public BankAddVo setLevel(String level) {
+		this.level = level;
 		return this;
 	}
 	public String getQedituse() {
@@ -54,53 +61,53 @@ public class BankAddVo {
 		this.qedituse = qedituse;
 		return this;
 	}
-	public String getQname() {
-		return qname;
+	public String getQuestVal() {
+		return questVal;
 	}
-	public BankAddVo setQname(String qname) {
-		this.qname = qname;
+	public BankAddVo setQuestVal(String questVal) {
+		this.questVal = questVal;
 		return this;
 	}
-	public boolean getTextCheck() {
+	public boolean isTextCheck() {
 		return textCheck;
 	}
 	public BankAddVo setTextCheck(boolean textCheck) {
 		this.textCheck = textCheck;
 		return this;
 	}
-	public String getQtext() {
-		return qtext;
+	public String getTextDisplay() {
+		return textDisplay;
 	}
-	public BankAddVo setQtext(String qtext) {
-		this.qtext = qtext;
+	public BankAddVo setTextDisplay(String textDisplay) {
+		this.textDisplay = textDisplay;
 		return this;
 	}
-	public boolean getCommentCheck() {
-		return commentCheck;
+	public boolean isComentCheck() {
+		return comentCheck;
 	}
-	public BankAddVo setCommentCheck(boolean commentCheck) {
-		this.commentCheck = commentCheck;
+	public BankAddVo setComentCheck(boolean comentCheck) {
+		this.comentCheck = comentCheck;
 		return this;
 	}
-	public boolean getSourceCheck() {
+	public String getComentDisplay() {
+		return comentDisplay;
+	}
+	public BankAddVo setComentDisplay(String comentDisplay) {
+		this.comentDisplay = comentDisplay;
+		return this;
+	}
+	public boolean isSourceCheck() {
 		return sourceCheck;
 	}
 	public BankAddVo setSourceCheck(boolean sourceCheck) {
 		this.sourceCheck = sourceCheck;
 		return this;
 	}
-	public String getQcomment() {
-		return qcomment;
+	public String getSourceDisplay() {
+		return sourceDisplay;
 	}
-	public BankAddVo setQcomment(String qcomment) {
-		this.qcomment = qcomment;
-		return this;
-	}
-	public String getQsource() {
-		return qsource;
-	}
-	public BankAddVo setQsource(String qsource) {
-		this.qsource = qsource;
+	public BankAddVo setSourceDisplay(String sourceDisplay) {
+		this.sourceDisplay = sourceDisplay;
 		return this;
 	}
 	public String getQuseyn() {
@@ -124,25 +131,26 @@ public class BankAddVo {
 		this.qdate = qdate;
 		return this;
 	}
-	public int getCcode() {
-		return ccode;
+	public int getcCode() {
+		return cCode;
 	}
-	public BankAddVo setCcode(int ccode) {
-		this.ccode = ccode;
+	public BankAddVo setcCode(int cCode) {
+		this.cCode = cCode;
 		return this;
 	}
 	@Override
 	public String toString() {
-		return "BankAddVo [qno=" + qno + ", qcode=" + qcode + ", qtype="
-				+ qtype + ", qlevel=" + qlevel + ", qedituse=" + qedituse
-				+ ", qname=" + qname + ", textCheck=" + textCheck + ", qtext="
-				+ qtext + ", commentCheck=" + commentCheck + ", qcomment="
-				+ qcomment + ", sourceCheck=" + sourceCheck + ", qsource="
-				+ qsource + ", quseyn=" + quseyn + ", qwriter=" + qwriter
-				+ ", qdate=" + qdate + ", ccode=" + ccode + "]";
+		return "BankAddVo [qno=" + qno + ", qcode=" + qcode + ", typeSelector="
+				+ typeSelector + ", level=" + level + ", qedituse=" + qedituse
+				+ ", questVal=" + questVal + ", textCheck=" + textCheck
+				+ ", textDisplay=" + textDisplay + ", comentCheck="
+				+ comentCheck + ", comentDisplay=" + comentDisplay
+				+ ", sourceCheck=" + sourceCheck + ", sourceDisplay="
+				+ sourceDisplay + ", quseyn=" + quseyn + ", qwriter=" + qwriter
+				+ ", qdate=" + qdate + ", cCode=" + cCode + "]";
 	}
-
-
+	
+	
 	
 	
 }
