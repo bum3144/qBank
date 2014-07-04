7fc2712a5ab9aa862222021ef0ac4b071dc163ea
Ratist <ura0508@hanmail.net>
2014-07-04 12:56:30 +0900

    .

--- qBank/src/main/java/util/MailMail.java
+++ qBank/src/main/java/util/MailMail.java
@@ -1,24 +1,47 @@
 package util;
 
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+
 
 
 public class MailMail {
-	private MailSender mailSender;
+	
+	private JavaMailSender mailSender;
 	 
-	public void setMailSender(MailSender mailSender) {
+	public void setMailSender(JavaMailSender mailSender) {
 		this.mailSender = mailSender;
 	}
  
 	public void sendMail(String from, String to, String subject, String msg) {
  
-		SimpleMailMessage message = new SimpleMailMessage();  
- 
-		message.setFrom(from);
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(msg);
-		mailSender.send(message);	
+		MimeMessage message = mailSender.createMimeMessage();  
+		
+  try{
+  	
+		message.setSubject(subject , "UTF-8");
+		message.setText(msg, "UTF-8", "html");
+		message.setFrom(new InternetAddress(from));
+		message.addRecipient(RecipientType .TO, new InternetAddress(to));
+		
+		mailSender.send(message);
+		
+	  } catch (MessagingException e){
+		e.printStackTrace();
+		return;
+	} catch (MailException e) {
+    e.printStackTrace();
+    return;
+   }
+  
 	}
 }
+
